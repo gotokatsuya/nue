9be99f3b72985d9531e6c92feedee43d689e320e
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-06-25 12:48:09 +0000

    Delete useless imported package

--- util.go
+++ util.go
@@ -1,11 +1,5 @@
 package nue
 
-import "strings"
-import "fmt"
-
-var _ = strings.Split
-var _ = fmt.Println
-
 func splitURLPath(path string) (prefix, pattern string) {
 	for i := 1; i < len(path); i++ {
 		if path[i] == '/' {
